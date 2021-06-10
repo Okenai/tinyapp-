8cff5ef2d71cd9a3871952b1039d3b9f5ebd84cd
Lana <kassembayeva@gmail.com>
2021-06-10 22:34:32 +0000

    the helper function was moved to separate file

---
+++ helpers.js
@@ -0,0 +1,11 @@
+const getUserByEmail = (email, database) => {
+  for (let id in database) {
+    const user = database[id];
+    if (user.email === email) {
+      return user
+    }
+  }
+  return null;
+};
+
+module.exports = {getUserByEmail};
