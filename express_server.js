a9c9517b7699716d70ed08be47ea7105ef176ba7
Lana <kassembayeva@gmail.com>
2021-06-11 02:51:39 +0000

    the helper function was tested and all changes necessary for the functionality were added

--- express_server.js
+++ express_server.js
@@ -51,13 +51,18 @@ app.get("/home", (req, res) => {
   };
   res.render("home", templateVars);
 });
+
 app.get("/", (req, res) => {
   const userId = req.session.id;
+  if (!userId) {
+    res.redirect('/login')
+  }
   const templateVars = {
     urls: urlDatabase,
     user: users[userId]
   };
-  res.render("home", templateVars);
+ 
+  res.redirect('/urls');
 });
 
 const urlsForUser = function (id) {
@@ -99,22 +104,35 @@ app.post("/urls", (req, res) => {
   }
 
   urlDatabase[tempShortUrl] = { longURL, userId };
-
+ 
   res.redirect(`/urls/${tempShortUrl}`);
 });
 
 app.get("/urls/:shortURL", (req, res) => {
+  
   const userId = req.session.id;
+  const shortURL = req.params.shortURL;
+  
+  if (!urlsForUser(userId).hasOwnProperty(shortURL)) {
+    return res.status(401).send("Sorry you are not authorised")
+  }
   const templateVars = {
-    shortURL: req.params.shortURL,
-    longURL: urlDatabase[req.params.shortURL].longURL,
+    shortURL: shortURL,
+    longURL: urlDatabase[shortURL].longURL,
     user: users[userId]
   };
+  
   res.render("urls_show", templateVars);
 });
 
 app.get("/u/:shortURL", (req, res) => {
-  const longURL = urlDatabase[req.params.shortURL]['longURL'];
+  const userId = req.session.id;
+  const shortURL = req.params.shortURL;
+  if (!urlsForUser(userId).hasOwnProperty(shortURL)) {
+    return res.status(401).send("Sorry you are not authorised")
+  }
+
+  const longURL = urlDatabase[shortURL].longURL;
   res.redirect(longURL);
 
 });
@@ -156,7 +174,7 @@ app.post('/register', (req, res) => {
     return res.status(400).send("Please enter your email and password")
   }
 
-  if (getUserByEmail(email)) {
+  if (getUserByEmail(email, users)) {
     return res.status(400).send("The user is already registered")
   }
 
@@ -166,9 +184,7 @@ app.post('/register', (req, res) => {
     bcrypt.hash(password, salt, (err, hash) => {
       users[id] = { id, email, 
         password: hash };
-
         req.session.id = id
-      
         res.redirect("/urls");
     });
   });
@@ -186,12 +202,14 @@ app.post('/login', (req, res) => {
   let email = req.body.email;
   let password = req.body.password;
 
-  let user = getUserByEmail(email);
+  let user_ID = getUserByEmail(email, users);
+  let user = users[user_ID];
 
   if (!user) {
     return res.status(403).send("No user found.")
   }
   
+
   bcrypt.compare(password, user.password, (err, result) => {
     if (!result) {
       return res.status(403).send("Wrong password.")
--- helpers.js
+++ helpers.js
@@ -2,10 +2,26 @@ const getUserByEmail = (email, database) => {
   for (let id in database) {
     const user = database[id];
     if (user.email === email) {
-      return user
+      return id
     }
   }
   return null;
 };
 
 module.exports = {getUserByEmail};
+
+
+const testUsers = {
+  "userRandomID": {
+    id: "userRandomID", 
+    email: "user@example.com", 
+    password: "purple-monkey-dinosaur"
+  },
+  "user2RandomID": {
+    id: "user2RandomID", 
+    email: "user2@example.com", 
+    password: "dishwasher-funk"
+  }
+};
+
+console.log(getUserByEmail("user@example.com", testUsers))
